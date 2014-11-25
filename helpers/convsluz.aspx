373c1bd6e704ddf70e136a80614919c15d77601b
Boris Duchaj <bduchaj@gmail.com>
2014-11-25 15:31:57 +0100

    konverzia sluzieb

---
+++ helpers/convsluz.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="convsluz.aspx.cs" Inherits="helpers_convsluz" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>Mesiac:<asp:TextBox ID="mesiac_txt" runat="server"></asp:TextBox></p>
+        <p>Rok:<asp:TextBox ID="rok_txt" runat="server"></asp:TextBox></p>
+        <asp:Button ID="run" runat="server" Text="Convert" OnClick="runConversion_fnc" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ helpers/convsluz.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class helpers_convsluz : System.Web.UI.Page
+{
+    public mysql_db x2MySql = new mysql_db();
+    public x2_var x2 = new x2_var();
+    public my_db x_db = new my_db();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void runConversion_fnc(object sender, EventArgs e)
+    {
+        int rok = Convert.ToInt32(this.rok_txt.Text.ToString());
+        int mesiac = Convert.ToInt32(this.mesiac_txt.ToString());
+
+
+        SortedList data_info = x_db.loadSluzbaMonthYear("is_sluzby", this.mesiac_txt.Text.ToString(), this.rok_txt.Text.ToString());
+
+        String[][] data = x2.parseSluzba(data_info["rozpis"].ToString());
+
+        int rows = data.Length;
+        int cols = data[0].Length;
+
+        ArrayList tmpValues = new ArrayList();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbDatum = new StringBuilder();
+
+        for (int den = 0; den < rows; den++)
+        {
+            sbDatum.AppendFormat("{0}-{1}-{2}",rok,mesiac,den+1);
+            string typ = "";
+            string userId = "";
+            for (int one=0; one<cols; one++)
+            {
+                if (one == 1)
+                {
+                    typ="OUP";
+                    string[] tmp= data[den][one].Split('|');
+                    userId = tmp[0].ToString();
+
+                }
+                if (one == 2)
+                {
+                    typ="OddA";
+                    userId = tmpValues[one];
+                }
+                sb.AppendFormat("([{0}],[{1}],[{2}],[{3}])",sbDatum.ToString(),typ,userId,"");
+        }
+
+
+
+    }
+}
