81541a3c1d3cfeb34d91ad3da69ebbfca7bfc267
Boris Duchaj <bduchaj@gmail.com>
2014-12-16 15:07:31 +0100

    praca na sluzbach uplne WoW

--- sluzby2.aspx.cs
+++ sluzby2.aspx.cs
@@ -20,84 +20,141 @@ public partial class sluzby2 : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        this.loadSluzby();
+
+        if (IsPostBack == false)
+        {
+            this.setMonthYear();
+
+            this.loadSluzby();
+        }
+        else
+        {
+           // this.loadSluzby();
+        }
     }
 
     protected void changeSluzba(object sender, EventArgs e)
     {
+        this.loadSluzby();
     }
+
+    protected void setMonthYear()
+    {
+        DateTime dnes = DateTime.Today;
+        int mesiac = dnes.Month;
+        int rok = dnes.Year;
+
+        this.mesiac_cb.SelectedValue = mesiac.ToString();
+        this.rok_cb.SelectedValue = rok.ToString();
+    }
+
+
 
     protected void loadSluzby()
     {
+        string mesiac = this.mesiac_cb.SelectedValue.ToString();
+        string rok = this.rok_cb.SelectedValue.ToString();
+        if (mesiac.Length == 1)
+        {
+            mesiac = "0" + mesiac;
+        }
+
         StringBuilder sb = new StringBuilder();
-        sb.Append("SELECT [t_sluzb].[datum] , GROUP_CONCAT([typ] ORDER BY ordering SEPARATOR ';') AS [type1],");
-        sb.Append("GROUP_CONCAT([user_id] ORDER BY ordering SEPARATOR ';') AS [users_id],");
-        sb.Append("GROUP_CONCAT(IF([user_id]=0,'-',[t_users].[full_name]) ORDER BY ordering SEPARATOR ';') AS [users_names],");
+        sb.Append("SELECT [t_sluzb].[datum] , GROUP_CONCAT([typ] ORDER BY [t_sluzb].[ordering] SEPARATOR ';') AS [type1],");
+        sb.Append("GROUP_CONCAT([t_sluzb].[user_id] ORDER BY [t_sluzb].[ordering] SEPARATOR ';') AS [users_id],");
+        sb.Append("GROUP_CONCAT(IF([t_sluzb].[user_id]=0,'-',[t_users].[full_name]) ORDER BY [t_sluzb].[ordering] SEPARATOR ';') AS [users_names],");
+        sb.Append("GROUP_CONCAT(IF([t_sluzb].[comment]=NULL,'-',[t_sluzb].[comment]) ORDER BY [t_sluzb].[ordering] SEPARATOR ';') AS [comment],");
         sb.Append("[t_sluzb].[date_group] AS [dategroup]");
         sb.Append("FROM [is_sluzby_2] AS [t_sluzb]");
         sb.Append("LEFT JOIN [is_users] AS [t_users] ON [t_users].[id] = [t_sluzb].[user_id]");
-        sb.AppendFormat("WHERE [t_sluzb].[date_group] = '{0}'","201411");
+        sb.AppendFormat("WHERE [t_sluzb].[date_group] = '{0}{1}'",rok,mesiac);
         sb.Append("GROUP BY [t_sluzb].[datum]");
         sb.Append("ORDER BY [t_sluzb].[datum]");
 
         Dictionary<int, SortedList> table = x2Mysql.getTable(sb.ToString());
 
-        string[] header = table[0]["type1"].ToString().Split(';');
 
-        int days = table.Count;
-        int colsNum = header.Length;
-
-        TableHeaderRow headRow = new TableHeaderRow();
-
-        TableHeaderCell headCell = new TableHeaderCell();
-        headCell.ID = "headCell_date";
-        headCell.Text = "Datum";
-        headRow.Controls.Add(headCell);
-
-        for (int head = 0; head < colsNum; head++)
+        if (table.Count > 0)
         {
-            TableHeaderCell headCell1 = new TableHeaderCell();
-            headCell1.ID = "headCell_" + head;
-            headCell1.Text = header[head].ToString();
-            headRow.Controls.Add(headCell1);
-        }
-        this.shiftTable.Controls.Add(headRow);
+            string[] header = table[0]["type1"].ToString().Split(';');
 
-        for (int row = 0; row < days; row++)
-        {
-            TableRow tblRow = new TableRow();
-            string[] names = table[row]["users_names"].ToString().Split(';');
+            int days = table.Count;
+            int colsNum = header.Length;
 
-            
-            TableCell cellDate = new TableCell();
-            cellDate.ID = "cellDate_" + row;
-            string text = (row + 1).ToString();
-            cellDate.Text = text;
-            tblRow.Controls.Add(cellDate);
-            for (int cols = 0; cols < colsNum; cols++)
+            TableHeaderRow headRow = new TableHeaderRow();
+
+            TableHeaderCell headCell = new TableHeaderCell();
+            headCell.ID = "headCell_date";
+            headCell.Text = "<strong>Datum</strong>";
+           // headCell.Style
+            headRow.Controls.Add(headCell);
+
+            for (int head = 0; head < colsNum; head++)
             {
-                TableCell dataCell = new TableCell();
-                dataCell.ID = "dataCell_" + row.ToString() + cols.ToString();
-                dataCell.Text = names[cols];
+                TableHeaderCell headCell1 = new TableHeaderCell();
+                headCell1.ID = "headCell_" + head;
+                headCell1.Text = "<strong>"+ header[head].ToString()+"</strong>";
+                headRow.Controls.Add(headCell1);
+            }
+            this.shiftTable.Controls.Add(headRow);
 
-                DateTime myDate = new DateTime(2014,11,row+1);
+            string[] freeDays = x2Sluzby.getFreeDays();
+            
+            for (int row = 0; row < days; row++)
+            {
+                TableRow tblRow = new TableRow();
+
+                string[] names = table[row]["users_names"].ToString().Split(';');
+                string[] userId = table[row]["users_id"].ToString().Split(';');
+                string[] comments = table[row]["comment"].ToString().Split(';');
+
+                DateTime myDate = new DateTime(Convert.ToInt32(rok), Convert.ToInt32(mesiac), row + 1);
                 int dnesJe = (int)myDate.DayOfWeek;
+                string nazov = CultureInfo.CurrentCulture.DateTimeFormat.DayNames[dnesJe];
+                string sviatok = (row + 1).ToString() + "." + mesiac;
+                int jeSviatok = Array.IndexOf(freeDays, sviatok);
+          
 
+                TableCell cellDate = new TableCell();
+                cellDate.ID = "cellDate_" + row;
                 if (dnesJe == 0 || dnesJe == 6)
                 {
-                    dataCell.CssClass = "box red";
+                    cellDate.CssClass = "box red";
                 }
 
-                string[] freeDays = x2Sluzby.getFreeDays();
+                if (jeSviatok != -1 && dnesJe != 0 && dnesJe != 6)
+                {
+                    cellDate.CssClass = "box yellow";
+                }
+                string text = (row + 1).ToString();
+                cellDate.Text = text+". "+nazov;
+                tblRow.Controls.Add(cellDate);
+                for (int cols = 0; cols < colsNum; cols++)
+                {
+                    TableCell dataCell = new TableCell();
+                    dataCell.ID = "dataCell_" + row.ToString() + cols.ToString();
+                    names[cols] = names[cols].Replace(" ","<br>");
+                    dataCell.Text = names[cols];
 
+                    Label comment = new Label();
+                    comment.ID = "label_" + row.ToString() + cols.ToString();
+                    comment.Text = comments[cols];
 
+                    
 
+                    if (dnesJe == 0 || dnesJe == 6)
+                    {
+                        dataCell.CssClass = "box red";
+                    }
+                    if (jeSviatok != -1 && dnesJe != 0 && dnesJe != 6)
+                    {
+                        dataCell.CssClass = "box yellow";
+                    }
+                    tblRow.Controls.Add(dataCell);
+                }
+                this.shiftTable.Controls.Add(tblRow);
 
-                
-                tblRow.Controls.Add(dataCell);
             }
-            this.shiftTable.Controls.Add(tblRow);
-
         }
 
 
