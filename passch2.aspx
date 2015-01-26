5c9c55ae1e2b1565b12cc658b551c0f5b06ff365
Boris Duchaj <bduchaj@gmail.com>
2015-01-26 15:03:36 +0000

    zmena passch2 na samostatnu stranku

---
+++ passch2.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="passch2.aspx.cs" Inherits="passch2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>Zmena hesla do is.kdch.sk:</h2>
+                <hr />
+         <asp:Label ID="info_lbl" runat="server" Text="Label" Visible="False"></asp:Label><br /><br />
+        <asp:PlaceHolder ID="changePassw" runat="server">
+          
+                
+                
+                <table border="0">
+                <tr><td>Nové heslo:</td><td><asp:TextBox ID="passwd1" runat="server" TextMode="Password">
+
+                                            </asp:TextBox></td></tr>
+               <tr>
+               <td>Kontrola hesla:</td>
+               <td><asp:TextBox ID="passwd2" runat="server" TextMode="Password"></asp:TextBox></td>
+              </tr>
+                 </table>
+                <asp:Button ID="change_btn" runat="server" Text="Zmeň" 
+                    onclick="change_btn_Click" />
+               </asp:PlaceHolder>    
+                   
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ passch2.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class passch2 : System.Web.UI.Page
+{
+    x2_var my_x2 = new x2_var();
+    my_db x_db = new my_db();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["tuisegumdrum"] == null)
+        {
+            Response.Redirect("error.html");
+        }
+
+       
+    }
+
+    protected void change_btn_Click(object sender, EventArgs e)
+    {
+        string my_passwd1 = this.passwd1.Text.ToString();
+        string my_passwd2 = this.passwd2.Text.ToString();
+
+        if ((my_passwd1 != my_passwd2) || (my_passwd1.Length == 0) || (my_passwd2.Length == 0))
+        {
+            this.info_lbl.Visible = true;
+            this.info_lbl.Text = "Hesla nie sú rovnaké, alebo heslo nezadané !!!!!";
+        }
+        else
+        {
+            SortedList data = new SortedList();
+            //data.Add("id",Request.Cookies["user_id"].Value.ToString());
+            data.Add("passwd", my_x2.make_hash(my_passwd1));
+            this.info_lbl.Visible = true;
+            string res = x_db.update_row("is_users", data, Session["user_id"].ToString());
+            string wgroup = Session["workgroup"].ToString();
+
+            if (res == "ok")
+            {
+                if (wgroup == "doctor")
+                {
+                    this.info_lbl.Text = "Heslo bolo úspešne zmenené !!! <a href='hlasko.aspx' target='_self'> Začať pracovať</a>";
+                }
+                else
+                {
+                    this.info_lbl.Text = "Heslo bolo úspešne zmenené !!! <a href='sestrhlas.aspx' target='_self'> Začať pracovať</a>";
+                }
+
+                this.changePassw.Visible = false;
+            }
+            else
+            {
+                this.info_lbl.Text = "Chyba:" + res;
+            }
+
+        }
+
+    }
+
+}
