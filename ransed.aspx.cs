5967250948de7d6e1ffd908a72cf1bf0fad8d6d9
Boris Duchaj <bduchaj@gmail.com>
2015-05-16 07:34:48 +0200

    rozne upravy a pod

--- ransed.aspx.cs
+++ ransed.aspx.cs
@@ -59,9 +59,11 @@ public partial class ransed : System.Web.UI.Page
         this.note_txt.Text = "";
 
         this.loadSluzby();
-        this.loadKojenci();
-        this.loadDievcata();
-        this.loadChlapci();
+
+
+        //this.loadKojenci();
+        //this.loadDievcata();
+        //this.loadChlapci();
     }
 
 
@@ -113,121 +115,10 @@ public partial class ransed : System.Web.UI.Page
         }
         //sb.AppendLine("</ul>");
         meno_lnk.Text = sb.ToString();
-        this.sluzba_pl.Controls.Add(meno_lnk);
+       // this.sluzba_pl.Controls.Add(meno_lnk);
 
     }
 
-    protected void loadKojenci()
-    {
-        DateTime datum = this.Calendar1.SelectedDate;
-
-        StringBuilder sb = new StringBuilder();
-        sb.AppendFormat("SELECT * FROM [is_osirix] WHERE [date] = '{0}' AND [odd] = '{1}'", my_x2.unixDate(datum), "KOJ");
-
-        Dictionary<int, Hashtable> result = x2db.getTable(sb.ToString());
-        sb.Length = 0;
-        Label meno_lnk = new Label();
-        meno_lnk.ID = "kojenci";
-
-        //sb.AppendFormat("<ul>");
-
-        for (int i = 0; i < result.Count; i++)
-        {
-            sb.AppendFormat("<p><a class='blue button' href='http://10.10.2.49:3333/studyList?search={0}' target='_blank'>{0}</a>{1}</p>", result[i]["name"].ToString(), result[i]["poznamka"].ToString());
-
-        }
-       // sb.AppendFormat("</ul>");
-        meno_lnk.Text = sb.ToString();
-        this.kojenci_pl.Controls.Add(meno_lnk);
-    }
-
-    protected void loadDievcata()
-    {
-        DateTime datum = this.Calendar1.SelectedDate;
-
-        StringBuilder sb = new StringBuilder();
-        sb.AppendFormat("SELECT * FROM [is_osirix] WHERE [date] = '{0}' AND [odd] = '{1}'", my_x2.unixDate(datum), "MSV");
-
-        Dictionary<int, Hashtable> result = x2db.getTable(sb.ToString());
-        sb.Length = 0;
-        Label meno_lnk = new Label();
-        meno_lnk.ID = "dievacata";
-
-        //sb.AppendFormat("<ul>");
-
-        for (int i = 0; i < result.Count; i++)
-        {
-            sb.AppendFormat("<p><a class='blue button' href='http://10.10.2.49:3333/studyList?search={0}' target='_blank'>{0}</a>{1}</p>", result[i]["name"].ToString(), result[i]["poznamka"].ToString());
-
-        }
-       // sb.AppendFormat("</ul>");
-        meno_lnk.Text = sb.ToString();
-        this.dievcata_pl.Controls.Add(meno_lnk);
-    }
-
-
-    protected void loadChlapci()
-    {
-        DateTime datum = this.Calendar1.SelectedDate;
-
-        StringBuilder sb = new StringBuilder();
-        sb.AppendFormat("SELECT * FROM [is_osirix] WHERE [date] = '{0}' AND [odd] = '{1}'", my_x2.unixDate(datum), "VD");
-
-        Dictionary<int, Hashtable> result = x2db.getTable(sb.ToString());
-        sb.Length = 0;
-        Label meno_lnk = new Label();
-        meno_lnk.ID = "chlapci";
-
-        //sb.AppendFormat("<ul>");
-
-        for (int i = 0; i < result.Count; i++)
-        {
-            sb.AppendFormat("<p><a class='blue button' href='http://10.10.2.49:3333/studyList?search={0}' target='_blank'>{0}</a>{1}</p>", result[i]["name"].ToString(), result[i]["poznamka"].ToString());
-
-        }
-       // sb.AppendFormat("</ul>");
-        meno_lnk.Text = sb.ToString();
-        this.chlapci_pl.Controls.Add(meno_lnk);
-    }
-
-    protected void add_patient_click_fnc(object sender, EventArgs e)
-    {
-        SortedList data = new SortedList();
-
-        data["name"] = x2_var.UTFtoASCII(this.name_txt.Text.ToString());
-        data["poznamka"] = this.note_txt.Text.ToString();
-        data["odd"] = this.odd_dl.SelectedValue.ToString();
-        data["date"] = my_x2.unixDate(this.Calendar1.SelectedDate);
-
-        if (data["name"].ToString().Length > 0)
-        {
-            SortedList res = x2db.mysql_insert("is_osirix", data);
-
-            //SortedList res = x2db.mysql_insert("is_osirix", data);
-
-            bool status = Convert.ToBoolean(res["status"].ToString());
-
-            if (status == false)
-            {
-                this.alert("Chyba: " + res["msg"].ToString());
-            }
-            else
-            {
-                this.loadData();
-            }
-
-        }
-        else
-        {
-            this.msg_lbl.Text= "Meno nemoze byt prazdne....";
-        }
-
-       
-
-    }
-
-    protected void alert(string message)
-    {
-        Response.Write("<script>alert('" + message + "');</script>");
-    }
+    
+    
 }
